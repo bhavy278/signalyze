89bfb0f83750a99cbae3b79ea2d0198043e4a9d0
Bhavy Patel <bhavy0606@gmail.com>
2025-07-11 17:14:48 -0400

    removed all files

--- src/routes/ai.route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { handleOpenRouterAI } from "../controllers/ai.controller";
-
-const router = express.Router();
-
-router.post("/ans", handleOpenRouterAI);
-
-
-export default router;
